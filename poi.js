f94bdfc191b335aff044033e4cb2b7fa71c730f3
uj <tidusu@qq.com>
2019-04-28 15:51:16 +0800

    uj

---
+++ poi.js
@@ -0,0 +1,363 @@
+var poi = {
+        "type": "FeatureCollection",
+        "name": "kyoto",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+                "type": "Feature",
+                "properties": {
+                    "Name": "Grand Japaning Hotel Nijo",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.738121, 35.011244, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "Hotel WBF Namba Motomachi",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.496161199999989, 34.6620917, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "京都甘樂酒店",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.759435, 34.993566, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "金阁寺",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7292431, 35.03937, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "Hotel IL Monte",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.5020532, 34.7035883, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "Vermillion - espresso bar & info.",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.770849, 34.967553, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "老松 - Oimatsu",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7616722, 35.0044243, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "名代豬排",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.766788, 35.0038611, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "HARBS 京都藤井大丸店",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7665158, 35.0030006, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "% ARABICA Kyoto Fujii Daimaru",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.766698, 35.003348, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "Ohagi no Tanbaya",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7732624, 35.0039815, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "Yakiniku Hiro",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7609002, 34.9887373, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "燒肉名店 弘",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7709554, 35.0096947, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "Kyoto Yakiniku (BBQ) HIRO Senbon Sanjo Location",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7433816, 35.0084557, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "牛カツ 京都勝牛 河原町店",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7684046, 35.005954, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "京都勝牛 祇園八坂",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.777492099999989, 35.0023239, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "炸牛排 京都勝牛 京都站前店",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.760635, 34.989351, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "TORIKIZOKU (6F)",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7594843, 34.9889469, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "Torikizoku Karasuma",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7638029, 35.0038578, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "Torikizoku Kiyamachi-dori",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7706526, 35.0069541, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "鳥貴族四条大宮店",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.748426, 35.003937, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "蛋包飯 洋食屋 キチキチ",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7708108, 35.0072788, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "Omurahausu ",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7748067, 35.029769, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "炸蝦蛋花烏龍麵 岡北",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7848234, 35.014363, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "烏龍麵 山元麵藏",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7848267, 35.0142903, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "迴轉壽司 むさし",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.768750899999986, 35.0089572, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "牛排丼 Red Rock",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.767760900000013, 35.0075866, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "炭櫓 京都四条河原町店",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7696444, 35.0035297, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "Amairo Cafe Taiyaki",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.758645, 35.000869, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "抹茶館",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.769472400000012, 35.0044905, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "清水寺",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7850463, 34.9948561, 0.0]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "Name": "蟹肉棒",
+                    "icon": "food"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [135.7765287, 34.9944215, 0.0]
+                }
+            }
+        ]
+    }
